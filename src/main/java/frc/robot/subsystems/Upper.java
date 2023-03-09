84f03d7244f23ad8860a135e9a04dda30f00f335
Chenry <henrychen0214@gmail.com>
2023-03-09 11:03:43 -0800

    builds

--- src/main/java/frc/robot/subsystems/Upper.java
+++ src/main/java/frc/robot/subsystems/Upper.java
@@ -1,17 +1,11 @@
 package frc.robot.subsystems;
 
-import javax.swing.text.StyledEditorKit.BoldAction;
-
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FollowerType;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.sensors.AbsoluteSensorRange;
 import com.ctre.phoenix.sensors.CANCoder;
 import com.ctre.phoenix.sensors.SensorInitializationStrategy;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -41,11 +35,8 @@ public class Upper extends SubsystemBase{
     private double currentElbowTarget = 0;
     private double currentStringTarget = 0;
 
-    boolean isIdle = true;
-
     boolean stringSettled = true;
     boolean elbowSettled = true;
-    boolean rotationSettled = true;
 
 
     private static class elbowPos{
@@ -53,6 +44,7 @@ public class Upper extends SubsystemBase{
         static double high = 0;
         static double down = 0;
         static double human = 0;
+        static double placingOffset = 0;
     }
 
     private static class stringPos{
@@ -62,10 +54,16 @@ public class Upper extends SubsystemBase{
         static double human = 0;
     }
 
-    boolean grabFromHuman = false;
-    boolean scoreMid = false;
-    boolean scoreHigh = false;
-    boolean down = false;
+    enum States {
+        placing,
+        human,
+        coneHigh,
+        coneMid,
+        standby,
+    }
+
+
+    States state = States.standby;
 
 
     Upper(){
@@ -78,104 +76,51 @@ public class Upper extends SubsystemBase{
         elbowEncoder.configSensorInitializationStrategy(SensorInitializationStrategy.BootToAbsolutePosition);
         //can add invert
 
-        elbowPID = new PID(0.001, 0, 0, 0, 0);
+        elbowPID = new PID(0.0001, 0, 0, 0, 0);
         
         stringboi = new WPI_TalonFX(Constants.Superstructure.talonStringPort);
         stringPID = new PID(0, 0, 0, 0, 0);
 
 
         squishyboi = new Solenoid(PneumaticsModuleType.CTREPCM, Constants.Superstructure.solenoidPort);
-        
-
     }
     
     @Override
     public void periodic() {
-
-        //fix overwriting issue with multiple being true;
-        // if (grabFromHuman){
-        //     setElbowTarget(elbowPos.human);
-        //     elbowSettled = false;
-        //     if (elbowSettled) {
-        //         setStringTarget(stringPos.human);
-        //         stringSettled = false;
-        //     }
-        //     setRotationTarget(rotationPos.horizontal);
-        //     if (elbowSettled && stringSettled && rotationSettled) grabFromHuman = false;
-        // }
-
-        // if (down){
-        //     setRotationTarget(rotationPos.standby);
-        //     setStringTarget(stringPos.down);
-        //     if (rotationSettled && stringSettled) {setElbowTarget(elbowPos.down); down = false;}
-        // }
-
-        // if (scoreMid){
-        //     setElbowTarget(elbowPos.mid);
-        //     elbowSettled = false;
-        //     if (elbowSettled){
-        //         setStringTarget(stringPos.mid);
-        //         stringSettled = false;
-        //     }
-        //     if (stringSettled) {
-        //         setRotationTarget(rotationPos.horizontal);
-        //         scoreMid = false;
-        //     }
-        // }
-
-        // if (scoreHigh){
-        //     setElbowTarget(elbowPos.high);
-        //     elbowSettled = false;
-        //     if (elbowSettled){
-        //         setStringTarget(stringPos.high);
-        //         setRotationTarget(rotationPos.horizontal);
-        //         scoreHigh = false;
-        //     }
-        // }
-
-
-
-
-
         double stringError = currentStringTarget - stringEncoder.getPosition();
         double elbowError = currentElbowTarget - elbowEncoder.getAbsolutePosition();
+        
+        switch (state){
+            case standby:
+                setStringTarget(stringPos.down);
+                if (stringSettled) setElbowTarget(elbowPos.down);;
+                break;
+            case coneHigh:
+                setElbowTarget(elbowPos.high);
+                if (elbowSettled) setStringTarget(stringPos.high);
+                break;
+            case coneMid:
+                setElbowTarget(elbowPos.mid);
+                if (elbowSettled) setStringTarget(stringPos.mid);
+                break;
+            case human:
+                setElbowTarget(elbowPos.human);
+                setStringTarget(stringPos.human);
+                break;
+            case placing:
+                setElbowTarget(currentElbowTarget + elbowPos.placingOffset);
+                break;
+        }
+
+
         updateStates(stringError, elbowError);
         SmartDashboard.putNumber("elbow abs pos", elbowEncoder.getAbsolutePosition());
+        SmartDashboard.putNumber("string enc pos", stringEncoder.getPosition());
 
-        //rotationSet(rotationPID.calculate(rotationError));
         //stringSet(stringPID.calculate(stringError));
         //elbowSet(elbowPID.calculate(elbowError));
     }
 
-    // void standbyIntakeSequence(){
-    //     if (!isIdle) return;
-    //     isIdle = false;
-    //     boolean running = true;
-    //     rotationSetTarget(rotationPos.standby);
-    //     stringSetTarget(stringPos.standbyIntake);
-    //     elbowSetTarget(elbowPos.standbyIntake);
-    //     unClamp();
-    //     running = false;
-    //     if (!running) isIdle = true;
-    // }
-
-    // void scoreStandbySequence(){
-        
-    //     isIdle = false;
-    //     rotationSetTarget(rotationPos.standby);
-    //     stringSetTarget(stringPos.standbyIntake);
-    //     if (stringSettled) elbowSetTarget(elbowPos.standbyIntake);
-    //     if (stringSettled && elbowSettled) clamp();
-    //     elbowSetTarget(elbowPos.standbyPut);
-    //     isIdle = true;
-    // }
-
-    // void scoreSequence(){
-    //     isIdle = false;
-    //     elbowSetTarget(elbowPos.up);
-    //     if (elbowSettled) stringSetTarget(stringPos.up);
-    //     isIdle = true;
-    // }
 
     void updateStates(double stringError, double elbowError){
         stringSettled = stringSU.isSettled(stringError);
