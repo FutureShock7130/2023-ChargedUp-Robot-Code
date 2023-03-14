2ff305200281b8371659eb22e0ce601822e7b0e4
Shih28 <107164255+Shih28@users.noreply.github.com>
2023-03-14 15:29:32 -0700

    add auto

---
+++ src/main/java/frc/robot/autos/turnForTime.java
@@ -0,0 +1,45 @@
+package frc.robot.autos;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Swerve;
+
+public class turnForTime extends CommandBase {
+    private Swerve drive;
+    double time; 
+    double TotalTime;
+    double rotate;
+    public turnForTime(Swerve s_Swerve, double timeS, double r ){
+      drive = s_Swerve;
+      TotalTime = timeS;
+      rotate = r;
+      addRequirements(s_Swerve);
+    }
+  
+    public void initialize(){
+      time = Timer.getFPGATimestamp();
+    }
+    double dt = Timer.getFPGATimestamp() - time;
+    
+    public void execute(){
+  
+          drive.drive(new Translation2d(0, 0), rotate, false, true);
+          dt = Timer.getFPGATimestamp() - time;
+  
+    }
+  
+    public void end(boolean interrupted){
+      drive.drive(new Translation2d(0, 0), 0, false, true);
+    }
+  
+    public boolean isFinished(){
+        if(dt > TotalTime){
+            drive.drive(new Translation2d(0, 0), 0, false, true);
+            return true;
+            
+        }
+        else return false;
+    }
+    
+}
