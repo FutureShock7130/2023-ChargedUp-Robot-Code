18be6e2cfadac3bd3c5d60c8afc587a34c0141c8
Hong <hongnai4507@gmail.com>
2023-03-15 13:30:17 +0800

    Optimize intake

--- src/main/java/frc/robot/subsystems/Index.java
+++ src/main/java/frc/robot/subsystems/Index.java
@@ -4,7 +4,6 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
@@ -12,7 +11,6 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -31,13 +29,14 @@ public class Index extends SubsystemBase {
   private WPI_TalonFX tilter = new WPI_TalonFX(intake.tilterPort, "7130");
   private DigitalInput limitSwitch = new DigitalInput(1);
 
-  private PID tilterPID = new PID(0.00005, 0, 0.00005, 0, 0);
-  private Timer time;
+  private PID tilterPID = new PID(0.00007, 0, 0.00005, 0, 0);
+  private PID upPID = new PID(0.0003, 0, 0.00022, 0, 0);
 
   private boolean isClamped = true;
   private boolean shoot = false;
   private indexStates state = indexStates.Standby;
   private indexPos desirePos = indexPos.Up;
+  private indexStates lastState;
 
   public enum indexStates {
     Indexing,
@@ -78,14 +77,17 @@ public class Index extends SubsystemBase {
     updateStates();
     if (shoot) shoot(-1);
     if (limitSwitch.get() && tilter.getSelectedSensorVelocity() > 0) tilter.set(0);
-    if (tilter.getSelectedSensorPosition() <= tilterPos.downlimit && tilter.getSelectedSensorVelocity() < 0) tilter.set(0);
-    if (desirePos == indexPos.Up && !limitSwitch.get() && tilterPos.upLimit - currentTilterPos < 2500) tilter.set(0.15);
 
     SmartDashboard.putBoolean("Intake isClamped", isClamped);
     SmartDashboard.putBoolean("Intake atUpLimit", limitSwitch.get());
     SmartDashboard.putString("Intake state", state.toString());
     SmartDashboard.putNumber("Intake Pos", tilter.getSelectedSensorPosition());
     SmartDashboard.putNumber("Intake Vel", tilter.getSelectedSensorVelocity());
+  }
+
+  void setTilter(double speed) {
+    if (tilter.getSelectedSensorPosition() <= tilterPos.downlimit && tilter.getSelectedSensorVelocity() < 0) tilter.set(0);
+    tilter.set(speed);
   }
 
   public void setRollers(double speed) {
@@ -119,37 +121,36 @@ public class Index extends SubsystemBase {
 
   public void setTilterPosBySpd(double speed) {
     if (speed > 0 && limitSwitch.get()) 
-      tilter.set(ControlMode.PercentOutput, 0);
-    // else if (speed < 0 && tilter.getSelectedSensorPosition() < tilterPos.downlimit) 
-    //   tilter.set(ControlMode.PercentOutput, 0);
+      setTilter(0);
     else if (tilter.getSelectedSensorPosition() > tilterPos.upSmoothPoint) 
-      tilter.set(ControlMode.PercentOutput, MathUtility.clamp(speed * 0.4, -1, 1));
+      setTilter(MathUtility.clamp(speed * 0.4, -1, 1));
     else if (tilter.getSelectedSensorPosition() < tilterPos.downlimit && speed < 0)
-      tilter.set(ControlMode.PercentOutput, 0);
+      setTilter(0);
     else 
-      tilter.set(ControlMode.PercentOutput, MathUtility.clamp(speed, -1, 1));
+      setTilter(MathUtility.clamp(speed, -1, 1));
   }
 
-  public void stick() {
-    double setpoint = 0;
-    double error = setpoint - tilter.getSelectedSensorPosition();
-    double output = tilterPID.calculate(error);
-    tilter.set(ControlMode.PercentOutput, MathUtility.clamp(output, -0.17, 0.17));
-  }
+  // public void stick() {
+  //   double setpoint = 0;
+  //   double error = setpoint - tilter.getSelectedSensorPosition();
+  //   double output = tilterPID.calculate(error);
+  //   tilter.set(ControlMode.PercentOutput, MathUtility.clamp(output, -0.17, 0.17));
+  // }
 
   public void setTilterPosAuto(indexPos pos) {
     switch (pos) {
       case Up:
         desirePos = indexPos.Up;
-        tilter.set(ControlMode.PercentOutput, MathUtility.clamp(tilterPID.calculate(tilterPos.upLimit - currentTilterPos), -0.3, 0.3));
+        if (lastState == indexStates.Indexing) setTilter(MathUtility.clamp(upPID.calculate(tilterPos.upLimit - currentTilterPos), -0.3, 0.3));
+        else setTilter(MathUtility.clamp(tilterPID.calculate(tilterPos.upLimit - currentTilterPos), -0.3, 0.3));
         break;
       case Down:
         desirePos = indexPos.Down;
-        tilter.set(ControlMode.PercentOutput, MathUtility.clamp(tilterPID.calculate(tilterPos.downlimit - currentTilterPos), -0.27, 0.27));
+        setTilter(MathUtility.clamp(tilterPID.calculate(tilterPos.downlimit - currentTilterPos), -0.3, 0.3));
         break;
       case GridCom:
         desirePos = indexPos.GridCom;
-        tilter.set(ControlMode.PercentOutput, MathUtility.clamp(tilterPID.calculate(tilterPos.inCom - currentTilterPos), -0.17, 0.17));
+        setTilter(MathUtility.clamp(tilterPID.calculate(tilterPos.inCom - currentTilterPos), -0.17, 0.17));
       default:
         break;
     }
@@ -161,32 +162,37 @@ public class Index extends SubsystemBase {
         setTilterPosAuto(indexPos.Down);
         setRollers(0.28);
         if (MathUtility.isWithin(tilter.getSelectedSensorPosition(), tilterPos.downlimit - 1000, tilterPos.downlimit + 1000)) unclamp();
+        lastState = indexStates.Indexing;
         break;
       case IndexHuman:
         clamp();
         setRollers(0.28);
         setTilterPosAuto(indexPos.Up);
+        lastState = indexStates.IndexHuman;
         break;
       case AimTop:
         clamp();
         setRollers(0);
         setTilterPosAuto(indexPos.GridCom);
+        lastState = indexStates.AimTop;
         break;
       case AimMiddle:
         clamp();
         setRollers(0);
         setTilterPosAuto(indexPos.GridCom);
+        lastState = indexStates.AimMiddle;
         break;
       case AimBottom:
         clamp();
         setRollers(0);
         setTilterPosAuto(indexPos.GridCom);
+        lastState = indexStates.AimBottom;
         break;
       case Standby:
         clamp();
         setRollers(0);
-        // setRollers(0.038);
         setTilterPosAuto(indexPos.Up);
+        lastState = indexStates.Standby;
       default:
         break;
     }
