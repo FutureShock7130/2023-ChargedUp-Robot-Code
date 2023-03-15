28aa2a57b776520a2b1822cb47d52b5cb57b2023
Shih28 <107164255+Shih28@users.noreply.github.com>
2023-03-14 17:30:54 -0700

    sdfghj

---
+++ src/main/java/frc/robot/autos/driveSide.java
@@ -0,0 +1,56 @@
+package frc.robot.autos;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.ChenryLib.PID;
+import frc.robot.subsystems.Swerve;
+
+public class driveSide extends CommandBase{
+    Swerve drive;
+    double targetDis;
+    double currentDistance;
+    double p, i, d, output;
+    PID sidePID;
+    double settle = 0.1;//😱😱😱😱
+
+    public driveSide(Swerve swerve, double targetDis, double kp, double ki, double kd){
+        drive = swerve;
+        this.targetDis = targetDis;
+        p = kp;
+        i = ki;
+        d = kd;
+        addRequirements(swerve);
+    }
+
+    @Override
+    public void initialize() {
+        sidePID = new PID(p, i, d, targetDis, 1);//😱😱😱😱😱
+    }
+
+    @Override
+    public void execute() {
+        currentDistance = drive.getPose().getY();
+        output = sidePID.calculate(targetDis - currentDistance);
+        // drive.drive(new Translation2d(0, output), 0, false, true);
+
+        SmartDashboard.putNumber("autoSideY", currentDistance);
+        SmartDashboard.putNumber("autoSideOutput", output);
+    }
+
+    @Override
+    public boolean isFinished() {
+        if(Math.abs(currentDistance - targetDis) <= settle){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        drive.drive(new Translation2d(0, 0), 0, false, false);
+    }
+
+
+}
